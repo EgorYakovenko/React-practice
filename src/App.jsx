4e8649c15ceb6b91071a12b506cd5fa63794a99e
EgorYakovenko <Egor.Yakovenko@ukr.net>
2024-03-01 17:35:36 +0100

    qwe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 // import './App.css'
 
 import LoginForm from './components/ModuleThreeForms/LoginForm';
@@ -8,20 +8,34 @@ import LangSwitcher from './components/ModuleThreeForms/LangSwitcher';
 function App() {
   // ===== Не контролируемая форма =====
 
-  const handleLogin = userData => {
-    // Виконуємо необхідні операції з даними
-    console.log(userData);
-  };
+  // const handleLogin = userData => {
+  // Виконуємо необхідні операції з даними
+  // console.log(userData);
+  // };
   // ===== /Не контролируемая форма =====
 
   // ===== Контролируемая форма =====
 
-  const [lang, setLang] = useState('uk'); // сохранили текущее значение состояния
+  // const [lang, setLang] = useState('uk'); // сохранили текущее значение состояния
 
   // ===== /Контролируемая форма =====
-  return (
+  // ===== HTTP запрос =====
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      const response = await axios.get(
+        'hn.algolia.com/api/v1/search?query=react'
+      );
+      setArticles(response.data.hits);
+    }
+    fetchArticles();
+  }, []);
+  // hn.algolia.com/api/v1/search?query=react
+  // nfETKchrhu0QZJvrk5wEPWVq-5iCKjXTqUbWwdRMBwo
+  http: return (
     <>
-      <h3>3 модуль, формы</h3>
+      {/* <h3>3 модуль, формы</h3>
       <h2>===== Не контролируемая форма =====</h2>
       <LoginForm onSubmit={handleLogin} />
       <p>===== /Не контролируемая форма =====</p>
@@ -30,7 +44,19 @@ function App() {
       <SearchBar />
 
       <p>Selected language: {lang}</p>
-      <LangSwitcher value={lang} onSelect={setLang} />
+      <LangSwitcher value={lang} onSelect={setLang} /> */}
+      <h3>HTTP запросы</h3>
+      {/* <div>
+        {articles.length > 0 && (
+          <ul>
+            {articles.map(article => {
+              <li key={article.objectID}>
+                <a href={article.url}>{article.title}</a>
+              </li>;
+            })}
+          </ul>
+        )}
+      </div> */}
     </>
   );
 }
